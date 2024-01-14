085577984ddb93329da9d181c474fa3fc739d648
Artur Avetisyan <artaveti@c2r15s3.42yerevan.am>
2024-01-14 18:42:02 +0400

    14.01.2024

--- sources/for_builtin_unset.c
+++ sources/for_builtin_unset.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   for_builtin_ft_wrong_name.c                        :+:      :+:    :+:   */
+/*   for_builtin_unset.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: artaveti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: artaveti <artaveti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 10:33:31 by artaveti          #+#    #+#             */
-/*   Updated: 2024/01/11 15:27:28 by artaveti         ###   ########.fr       */
+/*   Updated: 2024/01/14 18:39:37 by artaveti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	ft_wrong_name(char *name, char *command)
 	int	len;
 	int	i;
 
-	if (!(name[0] == '_' || ft_isalpha(name[0])) || !name || !(*name))
+	if (!(name[0] == '_' || ft_isalpha(name[0]))
+		|| !name || !(*name) || name == NULL)
 	{
 		printf(ERROR_WRONG_NAME, command, name);
 		return (1);
